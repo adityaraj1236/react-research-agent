e7df2d01153fb6bc2e2c078450da4094165a5913
Aditya Raj <adityaraj06072003@gmail.com>
2026-03-14 09:41:57 +0530

    tools completion

--- src/agent/resarchAgent.ts
+++ src/agent/resarchAgent.ts
@@ -2,11 +2,14 @@ import {createAgent} from  "langchain"
 import { llm } from "../llm/model";
 import { create } from "node:domain";
 import { SYSTEM_PROMPT } from "../llm/prompt";
+import { searchTool } from "../tools/searchTool";
+import { summariseTool } from "../tools/summariseTool";
+import { fetchTool } from "../tools/fetchTool";
 
 const agent  = createAgent({
-    llm,
-    tools: [],  
-    SYSTEM_PROMPT
+    model:  llm,
+    tools: [searchTool , fetchTool , summariseTool],  
+    systemPrompt :SYSTEM_PROMPT
 })
 
 export default agent;
